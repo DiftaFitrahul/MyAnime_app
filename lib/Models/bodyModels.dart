d9bcc1194a27c8e886029dffbab86eb028d135dd
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2022-12-29 11:52:03 +0700

    feat: finish my own style to display data

---
+++ lib/Models/bodyModels.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:myanimeapp/Models/fetchDataAnime.dart';
+import 'package:myanimeapp/Service/anime.dart';
+
+class AnimeList extends StatefulWidget {
+  const AnimeList({Key key}) : super(key: key);
+
+  @override
+  State<AnimeList> createState() => _AnimeListState();
+}
+
+class _AnimeListState extends State<AnimeList> {
+  Future<List<Anime>> futureAnime;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAnime = FetchDataAnime.fetchAnime();
+  }
+
+  Column dataAnimes(List<Anime> datas, int index) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 230,
+          width: 170,
+          child: Image(
+              fit: BoxFit.cover, image: NetworkImage(datas[index].animeImg)),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          datas[index].animeTitle,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("List of Popular Anime")),
+        body: Stack(
+          children: [
+            FutureBuilder<List<Anime>>(
+              future: futureAnime,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 10,
+                            crossAxisSpacing: 10,
+                            mainAxisExtent: 300),
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) {
+                      return dataAnimes(snapshot.data, index);
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return const CircularProgressIndicator();
+              },
+            ),
+          ],
+        ));
+  }
+}
--- lib/Models/fetchDataAnime.dart
+++ lib/Models/fetchDataAnime.dart
@@ -8,8 +8,8 @@ class FetchDataAnime {
     final response =
         await http.get(Uri.parse('https://gogoanime.consumet.org/popular'));
     if (response.statusCode == 200) {
-      final Animes = (jsonDecode(response.body)) as List;
-      return Animes.map((e) => Anime.fromJson(e)).toList();
+      final animes = (jsonDecode(response.body)) as List;
+      return animes.map((e) => Anime.fromJson(e)).toList();
     } else {
       throw Exception('Failed to load Animes');
     }

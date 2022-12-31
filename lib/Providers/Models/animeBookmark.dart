9f422c1ba37e85830b2c912e484eaf9eac23b98d
Difta Fitrahul Qihaj <difta.fit2002@mail.ugm.ac.id>
2022-12-31 21:55:05 +0700

    feat: create animeBookmark class

---
+++ lib/Providers/Models/animeBookmark.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AnimesBookmark {
+  final String title;
+  final String imageUrl;
+  final String releaseDate;
+  bool isDelete;
+
+  AnimesBookmark({this.title, this.imageUrl, this.releaseDate});
+}

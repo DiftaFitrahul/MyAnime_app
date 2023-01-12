8b6af5433a9c9141d9f4e007f92253e7acac93b7
Difta Fitrahul Qihaj <difta.fit2002@mail.ugm.ac.id>
2023-01-12 13:42:02 +0700

    feat: finish create gradation image

--- lib/Providers/screens/AnimeDetail.dart
+++ lib/Providers/screens/AnimeDetail.dart
@@ -13,7 +13,9 @@ class AnimeDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final animeId = ModalRoute.of(context)?.settings.arguments as String;
-    final anime = Provider.of<AnimesProvider>(context).dataAnimes.firstWhere(
+    final anime = Provider.of<AnimesProvider>(context, listen: false)
+        .dataAnimes
+        .firstWhere(
       (animes) {
         return animes.animeId == animeId;
       },
@@ -21,7 +23,46 @@ class AnimeDetailScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 2, 23, 56),
       appBar: AppBar(title: Text(anime.animeTitle)),
-      body: Center(child: Text('This is an Anime of index ${anime.animeImg}')),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.45,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: NetworkImage(anime.animeImg),
+                  ),
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.45,
+                foregroundDecoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [
+                          Colors.transparent,
+                          Color.fromARGB(255, 2, 23, 56)
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: [0.6, 1.0])),
+              ),
+              Positioned(
+                
+                child: Text(
+                  anime.animeTitle,
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
+          Center(child: Text('This is an Anime of index ${anime.animeImg}')),
+        ],
+      ),
     );
   }
 }

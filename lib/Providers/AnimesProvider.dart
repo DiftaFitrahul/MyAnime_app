80760d2f9ce76f2c96ae74403c4c231d8f93184d
Difta Fitrahul Qihaj <difta.fit2002@mail.ugm.ac.id>
2023-01-13 15:26:15 +0700

    feat: finish make horizontal listview

--- lib/Providers/AnimesProvider.dart
+++ lib/Providers/AnimesProvider.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import "dart:math";
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,9 +8,11 @@ import './Models/anime.dart';
 import 'package:http/http.dart' as http;
 
 class AnimesProvider with ChangeNotifier {
+  Random random = Random();
   List<Anime> _dataAnimes = [];
   List<Anime> get dataAnimes => _dataAnimes;
   bool isLoading = false;
+  List<Anime> animeSelection = [];
 
   Future<void> getAllAnimes() async {
     isLoading = true;
--- lib/Providers/screens/AnimeDetail.dart
+++ lib/Providers/screens/AnimeDetail.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import "dart:math";
+import 'package:flutter_lorem/flutter_lorem.dart';
 import 'package:myanimeapp/Providers/AnimesProvider.dart';
 import 'package:provider/provider.dart';
 import 'package:readmore/readmore.dart';
+
+import '../Models/anime.dart';
 
 class AnimeDetailScreen extends StatelessWidget {
   static const routeName = '/Anime-detail';
@@ -22,140 +23,215 @@ class AnimeDetailScreen extends StatelessWidget {
         return animes.animeId == animeId;
       },
     );
+    Random random = new Random();
+    List<Anime> todays_Selection = [
+      dataAnime.dataAnimes[1],
+      dataAnime.dataAnimes[3],
+      dataAnime.dataAnimes[5],
+      dataAnime.dataAnimes[7],
+      dataAnime.dataAnimes[2],
+      dataAnime.dataAnimes[4]
+    ];
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 2, 23, 56),
-      appBar: AppBar(title: Text(anime.animeTitle)),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
+        backgroundColor: const Color.fromARGB(255, 2, 23, 56),
+        appBar: AppBar(title: Text(anime.animeTitle)),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                height: MediaQuery.of(context).size.height * 0.41,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: NetworkImage(anime.animeImg),
-                  ),
-                ),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.41,
-                foregroundDecoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                        colors: [
-                          Colors.transparent,
-                          Color.fromARGB(255, 2, 23, 56)
-                        ],
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        stops: [0.44, 1.0])),
-              ),
-              Positioned(
-                left: 12,
-                bottom: 0,
-                width: 350,
-                child: Text(
-                  anime.animeTitle,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                      fontSize: 30,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                ),
-              )
-            ],
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 12, top: 8),
-            child: Text(
-              'Action, Adventure, Shounen, Magic, Historical',
-              style: TextStyle(color: Colors.grey),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 12, top: 15),
-            child: Row(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(right: 17),
-                  width: 160,
-                  height: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.amber[700],
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Material(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.transparent,
-                    child: InkWell(
-                      splashColor: Colors.amber.shade400,
-                      onTap: () {},
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 20, right: 10),
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: const [
-                              Text(
-                                'Play',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 25,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Icon(
-                                Icons.play_circle_outline_sharp,
-                                color: Colors.white,
-                                size: 40,
-                              )
-                            ]),
+              Stack(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.41,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: NetworkImage(anime.animeImg),
                       ),
                     ),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    dataAnime.favoriteStatusinDetailScreen(animeId);
-                  },
-                  style: ButtonStyle(
-                      padding: MaterialStateProperty.all(
-                          const EdgeInsets.symmetric(vertical: 8)),
-                      backgroundColor: MaterialStateProperty.all(Colors.black)),
-                  child: Consumer<AnimesProvider>(
-                    builder: (context, value, child) => Icon(
-                      value.dataAnimes
-                              .firstWhere(
-                                  (element) => element.animeId == animeId)
-                              .isFavorite
-                          ? Icons.favorite_outlined
-                          : Icons.favorite_border_outlined,
-                      color: Colors.red,
-                      size: 31,
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.41,
+                    foregroundDecoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [
+                              Colors.transparent,
+                              Color.fromARGB(255, 2, 23, 56)
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            stops: [0.44, 1.0])),
+                  ),
+                  Positioned(
+                    left: 12,
+                    bottom: 0,
+                    width: 350,
+                    child: Text(
+                      anime.animeTitle,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
                     ),
+                  )
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 12, top: 8),
+                child: Text(
+                  'Action, Adventure, Shounen, Magic, Historical',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 12, top: 15),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(right: 17),
+                      width: 160,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.amber[700],
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Material(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.transparent,
+                        child: InkWell(
+                          splashColor: Colors.amber.shade400,
+                          onTap: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 20, right: 10),
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: const [
+                                  Text(
+                                    'Play',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Icon(
+                                    Icons.play_circle_outline_sharp,
+                                    color: Colors.white,
+                                    size: 40,
+                                  )
+                                ]),
+                          ),
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        dataAnime.favoriteStatusinDetailScreen(animeId);
+                      },
+                      style: ButtonStyle(
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.symmetric(vertical: 8)),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.black)),
+                      child: Consumer<AnimesProvider>(
+                        builder: (context, value, child) => Icon(
+                          value.dataAnimes
+                                  .firstWhere(
+                                      (element) => element.animeId == animeId)
+                                  .isFavorite
+                              ? Icons.favorite_outlined
+                              : Icons.favorite_border_outlined,
+                          color: Colors.red,
+                          size: 31,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 17),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.symmetric(vertical: 8)),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.black)),
+                      child: const Icon(
+                        Icons.file_download_outlined,
+                        color: Colors.white,
+                        size: 31,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 12, top: 30),
+                child: Text(
+                  'Synopsis',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 12, top: 8),
+                child: SingleChildScrollView(
+                  child: ReadMoreText(
+                    lorem(paragraphs: 1, words: 100),
+                    style: const TextStyle(color: Colors.white70),
                   ),
                 ),
-                const SizedBox(width: 17),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                      padding: MaterialStateProperty.all(
-                          const EdgeInsets.symmetric(vertical: 8)),
-                      backgroundColor: MaterialStateProperty.all(Colors.black)),
-                  child: const Icon(
-                    Icons.file_download_outlined,
-                    color: Colors.white,
-                    size: 31,
-                  ),
+              ),
+              const Padding(
+                  padding: EdgeInsets.only(left: 12, top: 20),
+                  child: Text(
+                    "Today's Selection",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400),
+                  )),
+              SizedBox(
+                height: 260,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 12, top: 10),
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: todays_Selection.length,
+                      itemBuilder: ((context, index) => Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: 140,
+                                margin: const EdgeInsets.only(right: 10),
+                                child: Image(
+                                  fit: BoxFit.fill,
+                                  image: NetworkImage(
+                                      todays_Selection[index].animeImg),
+                                ),
+                              ),
+                              Container(
+                                width: 140,
+                                padding:
+                                    const EdgeInsets.only(right: 10, top: 5),
+                                child: Text(
+                                  todays_Selection[index].animeTitle,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              )
+                            ],
+                          ))),
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(
+                height: 100,
+              )
+            ],
           ),
-          SingleChildScrollView(
-            child: ReadMoreText(''),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }

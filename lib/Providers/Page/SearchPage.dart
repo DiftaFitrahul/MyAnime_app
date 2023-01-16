19e3a47a2ff8f414f292e00a6aba466e83b30db5
Difta Fitrahul Qihaj <difta.fit2002@mail.ugm.ac.id>
2023-01-16 21:41:24 +0700

    feat: finish make winter 2022 and readmore text

--- lib/Providers/Page/SearchPage.dart
+++ lib/Providers/Page/SearchPage.dart
@@ -22,135 +22,165 @@ class SearchPage extends StatelessWidget {
     ];
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 2, 23, 56),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
+      body: Column(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.only(top: 40, bottom: 20, left: 10, right: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width * 0.77,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.only(top: 12),
+                        hintText: 'Search',
+                        hintStyle:
+                            TextStyle(color: Colors.grey.withOpacity(0.7)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        filled: true,
+                        fillColor: const Color.fromARGB(255, 43, 66, 85),
+                        prefixIcon: const Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        )),
+                    onSubmitted: (value) {},
+                  ),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Stack(children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 49, 73, 92),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        minimumSize: Size.zero,
+                        padding: const EdgeInsets.all(10)),
+                    onPressed: (() {}),
+                    child: const Icon(Icons.notifications),
+                  ),
+                  Positioned(
+                    right: 16,
+                    top: 16,
+                    child: Container(
+                      padding: const EdgeInsets.all(2.0),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.red),
+                      constraints:
+                          const BoxConstraints(minHeight: 5, minWidth: 5),
+                    ),
+                  )
+                ])
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Popular Now",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 23),
+                ),
+                TextButton(onPressed: () {}, child: const Text('See More'))
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 380,
+            child: Padding(
               padding: const EdgeInsets.only(
-                  top: 40, bottom: 20, left: 10, right: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width * 0.77,
-                    child: TextField(
-                      decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.only(top: 12),
-                          hintText: 'Search',
-                          hintStyle:
-                              TextStyle(color: Colors.grey.withOpacity(0.7)),
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30)),
-                          filled: true,
-                          fillColor: const Color.fromARGB(255, 43, 66, 85),
-                          prefixIcon: const Icon(
-                            Icons.search,
-                            color: Colors.white,
-                          )),
-                      onSubmitted: (value) {},
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  Stack(children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 49, 73, 92),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15)),
-                          minimumSize: Size.zero,
-                          padding: const EdgeInsets.all(10)),
-                      onPressed: (() {}),
-                      child: const Icon(Icons.notifications),
-                    ),
-                    Positioned(
-                      right: 16,
-                      top: 16,
-                      child: Container(
-                        padding: const EdgeInsets.all(2.0),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10.0),
-                            color: Colors.red),
-                        constraints:
-                            const BoxConstraints(minHeight: 5, minWidth: 5),
+                  left: 15, right: 15, top: 10, bottom: 25),
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: popularNow.length,
+                  itemBuilder: ((context, index) {
+                    return Container(
+                      margin: const EdgeInsets.only(right: 20),
+                      height: 350,
+                      width: 230,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 280,
+                            width: 220,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Image(
+                                  fit: BoxFit.fill,
+                                  image:
+                                      NetworkImage(popularNow[index].animeImg)),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 12),
+                            child: Text(
+                              popularNow[index].animeTitle,
+                              style: const TextStyle(
+                                  fontSize: 20,
+                                  overflow: TextOverflow.ellipsis,
+                                  color: Colors.white),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 8),
+                            child: Text(
+                              'Action, History, Drama, Fantasy, Adventure',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  overflow: TextOverflow.ellipsis,
+                                  color: Color.fromARGB(255, 116, 116, 116)),
+                            ),
+                          )
+                        ],
                       ),
-                    )
-                  ])
-                ],
-              ),
+                    );
+                  })),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 15, right: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    "Popular Now",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 23),
-                  ),
-                  TextButton(onPressed: () {}, child: const Text('See More'))
-                ],
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Winter 2022",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 23),
+                ),
+                TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'See More',
+                    ))
+              ],
             ),
-            SizedBox(
-              height: 600,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 15, right: 15, top: 10),
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: popularNow.length,
-                    itemBuilder: ((context, index) {
-                      return Container(
-                        margin: const EdgeInsets.only(right: 20),
-                        height: 350,
-                        width: 230,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              height: 280,
-                              width: 220,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(15),
-                                child: Image(
-                                    fit: BoxFit.fill,
-                                    image: NetworkImage(
-                                        popularNow[index].animeImg)),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 12),
-                              child: Text(
-                                popularNow[index].animeTitle,
-                                style: const TextStyle(
-                                    fontSize: 20,
-                                    overflow: TextOverflow.ellipsis,
-                                    color: Colors.white),
-                              ),
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.only(top: 8),
-                              child: Text(
-                                'Action, History, Drama, Fantasy, Adventure',
-                                style: TextStyle(
-                                    fontSize: 14,
-                                    overflow: TextOverflow.ellipsis,
-                                    color: Color.fromARGB(255, 134, 134, 134)),
-                              ),
-                            )
-                          ],
-                        ),
-                      );
-                    })),
-              ),
-            )
-          ],
-        ),
+          ),
+          // Expanded(
+          //   child: ListView.builder(
+          //       itemCount: 5,
+          //       itemBuilder: ((context, index) {
+          //         return Container(
+          //           width: 100,
+          //           height: 100,
+          //           color: Colors.red,
+          //         );
+          //       })),
+          // )
+        ],
       ),
     );
   }

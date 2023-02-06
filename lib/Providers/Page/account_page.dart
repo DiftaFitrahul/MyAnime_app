8231d05e4683cee15b392ed901428e3a0cc74d1d
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-02-06 23:02:30 +0700

    fix: add plan in account page

--- lib/Providers/Page/account_page.dart
+++ lib/Providers/Page/account_page.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
 
-
 class AccountPage extends StatelessWidget {
   const AccountPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Search Page'),),
+      appBar: AppBar(
+        title: const Text('Search Page'),
+      ),
       body: Center(
         child: Column(
-          children: const [
-            Icon(Icons.search),
-            Text('Search')
-          ],
+          children: const [Icon(Icons.search), Text('Search')],
         ),
-      )
-      ,
+      ),
     );
   }
 }
+
+//lets create a ui of account page

db2f688e18354246c333f034c6e0a54d31db9542
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-03-07 22:37:37 +0700

    fix: change api url

--- lib/Providers/Service/fetch_data_anime.dart
+++ lib/Providers/Service/fetch_data_anime.dart
@@ -6,7 +6,7 @@ import '../Models/anime.dart';
 class FetchDataAnime {
   static Future<List<Anime>> fetchAnime() async {
     final response =
-        await http.get(Uri.parse('https://api.jikan.moe/v4/top/anime'));
+        await http.get(Uri.parse('https://api.jikan.moe/v4/anime'));
     if (response.statusCode == 200) {
       final animes = (jsonDecode(response.body)['data']) as List;
       return animes.map((e) => Anime.fromJson(e)).toList();
